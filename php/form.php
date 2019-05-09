c2fd6d6f6d5fee7333b81be84634fccf79cc359b
Douglas Herrera <douglas.herrera.821@my.csun.edu>
2019-05-08 22:49:49 -0700

    Update form.php

--- php/form.php
+++ php/form.php
@@ -2,7 +2,7 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: Douglas Herrera Portfolio'; 
+    $from = 'From: Douglas Herrera'; 
     $to = 'dougu1802@gmail.com'; 
     $subject = 'Hello';
     $human = $_POST['human'];
@@ -11,7 +11,7 @@
 				
     if ($_POST['submit']) {
     if (mail ($to, $subject, $body, $from)) { 
-		echo "Thank You!" . " -" . "<a href='#' style='text-decoration:none;color:#d00d2d;'> Return to Homepage</a>";
+		echo "Thank You!" . " -" . "<a href='https://dougu1802.github.io/ctva461-portfolio/' style='text-decoration:none;color:#d00d2d;'> Return to Homepage</a>";
     } else { 
         echo '<p>Something went wrong, go back and try again!</p>'; 
     }
